cd5683d163ac9ad02f38a7550d4c353a35285673
Greg Schafer <grschafer@gmail.com>
2013-10-24 20:04:26 +0000

    Add support for postgres

--- quassel-search.go
+++ quassel-search.go
@@ -22,6 +22,7 @@ import (
   "fmt"
   "database/sql"
   _ "github.com/mattn/go-sqlite3"
+  _ "github.com/lib/pq"
 )
 
 type resultSet struct {
@@ -49,6 +50,7 @@ func makeSender(rawSender string) sender {
 type message struct {
   MessageId int
   Time time.Time
+  Channel string
   Sender sender
   Text string
 }
@@ -56,31 +58,53 @@ type message struct {
 // Database
 var db *sql.DB
 
+func messagesFromRows(rows *sql.Rows) chan message {
+  messages := make(chan message)
+
+  go func() {
+    defer rows.Close()
+    defer close(messages)
+    for rows.Next() {
+      var messageid int
+      var msgTime interface{}
+      var convertedTime time.Time
+      var buffercname, msgSender, msg string
+      err := rows.Scan(&messageid, &buffercname, &msgSender, &msgTime, &msg)
+      if err != nil {
+        log.Print(err)
+      }
+      // sqlite returns date as int64, postgres returns as time.Time
+      switch msgTime := msgTime.(type) {
+      case int64:
+        convertedTime = time.Unix(msgTime, 0)
+      case time.Time:
+        convertedTime = msgTime
+      }
+      m := message{MessageId: messageid,
+                   Time: convertedTime,
+                   Channel: buffercname,
+                   Sender: makeSender(msgSender),
+                   Text: msg}
+      messages <- m
+    }
+  }()
+
+  return messages
+}
+
 // TODO: return error
 func searchResults(needle string) (resultSet) {
   sql_needle := "%" + needle + "%"
-  rows, err := db.Query("select messageid,buffercname,sender,time,type,message from backlog natural join sender natural join buffer where message like ?", sql_needle)
+  rows, err := db.Query("select messageid,buffercname,sender,time,message from backlog natural join sender natural join buffer where type = 1 and message like $1", sql_needle)
   // TODO: more error checking! return error to handler
   if err != nil {
     log.Fatal(err)
   }
-  defer rows.Close()
 
   results := resultSet{Needle: needle, ChannelResults: make([]channelResult, 0)}
   channels := make(map[string][]message)
-  for rows.Next() {
-    var messageid, msgtype int
-    var msgtime int64
-    var buffercname, msgSender, msg string
-    rows.Scan(&messageid, &buffercname, &msgSender, &msgtime, &msgtype, &msg)
-    if msgtype == 1 {
-      //fmt.Println(buffercname, msgtime, msgSender, msg)
-      m := message{MessageId: messageid,
-                   Time: time.Unix(msgtime, 0),
-                   Sender: makeSender(msgSender),
-                   Text: msg}
-      channels[buffercname] = append(channels[buffercname], m)
-    }
+  for m := range messagesFromRows(rows) {
+    channels[m.Channel] = append(channels[m.Channel], m)
   }
   for channel,messages := range channels {
     cr := channelResult{Channel: channel, Messages: messages}
@@ -103,7 +127,7 @@ var dirSort = map[int]string{
   1: "asc",
 }
 func messageContext(messageId int, linesToFetch int, direction int) []message {
-  row := db.QueryRow("select bufferid from backlog where messageid = ?", messageId)
+  row := db.QueryRow("select bufferid from backlog where messageid = $1", messageId)
   var bufferid int
   err := row.Scan(&bufferid)
   if err != nil {
@@ -111,40 +135,29 @@ func messageContext(messageId int, linesToFetch int, direction int) []message {
     log.Fatal(err)
   }
 
-  query := fmt.Sprintf(`select messageid,buffercname,sender,time,type,message
+  query := fmt.Sprintf(`select messageid,buffercname,sender,time,message
                         from backlog
                         natural join sender
                         natural join buffer
-                        where messageid %s ?
-                          and bufferid = ?
+                        where messageid %s $1
+                          and bufferid = $2
                           and type = 1
                         order by messageid %s
-                        limit ?`, dirComparators[direction], dirSort[direction])
+                        limit $3`, dirComparators[direction], dirSort[direction])
   rows, err := db.Query(query, messageId, bufferid, linesToFetch)
   if err != nil {
     log.Fatal(err)
   }
-  defer rows.Close()
 
   results := make([]message, 0)
-  for rows.Next() {
-    var messageid, msgtype int
-    var msgtime int64
-    var buffercname, msgSender, msg string
-    rows.Scan(&messageid, &buffercname, &msgSender, &msgtime, &msgtype, &msg)
-    if msgtype == 1 {
-      //fmt.Println(buffercname, msgtime, msgSender, msg)
-      m := message{MessageId: messageid,
-                   Time: time.Unix(msgtime, 0),
-                   Sender: makeSender(msgSender),
-                   Text: msg}
-      results = append(results, m)
-    }
+  for m := range messagesFromRows(rows) {
+    results = append(results, m)
   }
   return results
 }
 
 // Web server
+// TODO: accept var num args, return list of all
 func tmplPath(tmpl string) string {
   return path.Join("templates", tmpl)
 }
@@ -197,8 +210,10 @@ func ajaxContextHandler(w http.ResponseWriter, r *http.Request) {
 // Main
 
 func main() {
+  fmt.Println("don't commit db password to repo!")
   var err error
-  db, err = sql.Open("sqlite3", "quassel-storage.sqlite")
+  db, err = sql.Open("sqlite3", "/var/lib/quassel/quassel-storage.sqlite")
+  //db, err = sql.Open("postgres", "user=quassel password=secret dbname=quassel")
   if err != nil {
     log.Fatal(err)
   }
@@ -211,5 +226,5 @@ func main() {
   http.HandleFunc("/search/", searchHandler)
   http.HandleFunc("/context/", ajaxContextHandler)
   http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-  http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(":4243", nil)
 }
